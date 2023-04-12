f4816bf6f08707f9f661aa9b3905a0fd4f7b7239
Lucas Morais <lucas.morais23ti@gmail.com>
2023-04-11 23:01:33 -0300

    Projeto concluido

--- src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
+++ src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -12,9 +12,9 @@ public class ParkingControlApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingControlApplication.class, args);
 	}
+
 	@GetMapping("/")
 	public String index(){
-		return "Hello World";
+		return "Olá Mundo!";
 	}
-
 }
